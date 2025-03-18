699e390dddf475acc8da652a653de2bf8a8879d6
Polcia <witkowska.pola@gmail.com>
2025-03-18 13:25:12 +0100

    zadanie 2

--- lab03/zad2.py
+++ lab03/zad2.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
-import graphviz
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("./lab02/iris1.csv")
+df = pd.read_csv("./iris1.csv")
 
 (train_set, test_set) = train_test_split(df.values, train_size=0.7, random_state=13)
 
@@ -14,3 +14,12 @@ test_classes = test_set[:, 4]
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_inputs, train_classes)
+
+plt.figure(figsize=(15, 10))
+tree.plot_tree(clf,
+            #    feature_names=df.feature_names,
+            #    class_names=df.target_names,
+               filled=True, rounded=True)
+
+plt.savefig('iris_decision_tree.png', dpi=100)
+plt.show()
