963446fc5fa02cc92af63b1fcf381fdf9ac7160b
Polcia <witkowska.pola@gmail.com>
2025-03-21 12:38:46 +0100

    zadanie 2 dokonczone

--- lab03/zad2.py
+++ lab03/zad2.py
@@ -1,25 +1,48 @@
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import tree
+from sklearn.datasets import load_iris
 import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
-df = pd.read_csv("./iris1.csv")
+iris = load_iris()
 
-(train_set, test_set) = train_test_split(df.values, train_size=0.7, random_state=13)
-
-train_inputs = train_set[:, 0:4]
-train_classes = train_set[:, 4]
-test_inputs = test_set[:, 0:4]
-test_classes = test_set[:, 4]
+train_inputs, test_inputs, train_classes, test_classes = train_test_split(
+    iris.data, iris.target, train_size=0.7, random_state=13)
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(train_inputs, train_classes)
 
-plt.figure(figsize=(15, 10))
-tree.plot_tree(clf,
-            #    feature_names=df.feature_names,
-            #    class_names=df.target_names,
-               filled=True, rounded=True)
+# plt.figure(figsize=(15, 10))
+# tree.plot_tree(clf,
+#                feature_names=iris.feature_names,
+#                class_names=iris.target_names,
+#                filled=True, rounded=True)
 
-plt.savefig('iris_decision_tree.png', dpi=100)
-plt.show()
+# plt.savefig('iris_decision_tree.png', dpi=100)
+# plt.show()
+
+predictions = clf.predict(test_inputs)
+
+def accuracy():
+    good_predictions = 0
+    length = len(test_classes)
+
+    for i in range(length):
+        if predictions[i] == test_classes[i]:
+            good_predictions = good_predictions + 1
+
+    print(good_predictions) #43
+    print(good_predictions/length*100, "%") #95.6%
+
+accuracy()
+
+cm = confusion_matrix(test_classes, predictions)
+print(cm)
+
+"""
+[[14  0  0] Setosa
+ [ 0 12  0] Versicolour
+ [ 0  2 17]] Virginica
+ Czyli dwa razy pomylił się przy Virginica.
+ AI zgadło 43 razy a ja 41 razy.
+"""
