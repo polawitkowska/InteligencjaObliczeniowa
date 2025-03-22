0e65726e71bc42a5ca6a17a34f400a6acf4a4850
Polcia <witkowska.pola@gmail.com>
2025-03-22 14:54:23 +0100

    zadanie 2

---
+++ lab04/zad2.py
@@ -0,0 +1,27 @@
+from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+iris = load_iris()
+
+train_data, test_data, train_labels, test_labels = train_test_split(iris.data, iris.target, train_size=0.7, random_state=13)
+
+clf = MLPClassifier(solver='lbfgs', 
+                    alpha=1e-5,
+                    hidden_layer_sizes=(3, 3), 
+                    random_state=1)
+
+clf.fit(train_data, train_labels)
+
+predictions = clf.predict(test_data)
+accuracy = accuracy_score(test_labels, predictions)
+cm = confusion_matrix(test_labels, predictions)
+
+print(accuracy)
+print(cm)
+
+"""
+hidden_layer_sizes = (2) oraz (3) daje dokładność 26.7%
+hidden_layer_sizes = (3, 3) daje dokładność 97.8%
+"""
