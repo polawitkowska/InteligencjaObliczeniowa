5f2eeb179e4c1bb556ca2fb51df8273285403c47
Polcia <witkowska.pola@gmail.com>
2025-05-09 12:29:41 +0200

    zadanie 1

--- lab08/zad01.py
+++ lab08/zad01.py
@@ -1,33 +1,38 @@
-import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-# nltk.download('punkt_tab')
-# nltk.download('stopwords')
-# nltk.download('wordnet')
-
-lemmatizer = WordNetLemmatizer()
+from collections import Counter
+import matplotlib.pyplot as plt
 
 with open('./lab08/article.txt', 'r', encoding='utf-8') as file:
     article = file.read()
 
-stop_words = set(stopwords.words('english'))
+tokens = word_tokenize(article)
+# print(len(tokens)) #969
 
+stop_words = set(stopwords.words('english'))
 additional_stop_words = ["'s", "'re", "n't", "(", ")", "%", ",", ".", "``", "-", "''", "?", "'", ":"]
 for w in additional_stop_words:
     stop_words.add(w)
 
-tokens = word_tokenize(article)
-
-# print(len(tokens)) #969
-
 filtered_article = [w for w in tokens if not w.lower() in stop_words]
-# print(len(filtered_article)) #613
-# print(len(filtered_article)) #492
+# print(len(filtered_article)) #na początku: 613 po dodaniu dodatkowych słów: 492
 
-lemmatized_article = set()
-for w in filtered_article:
-    word = lemmatizer.lemmatize(w)
-    lemmatized_article.add(word)
+lemmatizer = WordNetLemmatizer()
 
-# print(len(lemmatized_article)) #285
+lemmatized_article = [lemmatizer.lemmatize(token) for token in filtered_article]
+# print(len(lemmatized_article)) #492
+
+word_counts = Counter(lemmatized_article)
+# print(word_counts)
+
+most_common = word_counts.most_common(10)
+words = [word for word, count in most_common]
+counts = [count for word, count in most_common]
+
+plt.figure(figsize=(12, 6))
+plt.bar(words, counts)
+plt.title('10 najczęściej występujących słów')
+plt.xlabel('Słowa')
+plt.ylabel('Liczba wystąpień')
+plt.show()
